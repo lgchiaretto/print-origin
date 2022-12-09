136650295a086f4ea792ce68e9a7c7ad720c7a80
Luiz Gustavo Chiaretto <luizgustavo@chiaretto.com.br>
2022-12-08 23:38:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php 
-if (!function_exists('getallheaders')){ 
-    function getallheaders() { 
-       $headers = ''; 
-       foreach ($_SERVER as $name => $value) { 
-            if (substr($name, 0, 5) == 'HTTP_') { 
-                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
-            } 
-       } 
-       return $headers; 
-    } 
-}
+print_r($_SERVER)
 ?>
